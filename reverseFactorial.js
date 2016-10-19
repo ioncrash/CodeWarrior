926a0264486c1009cc40178e917c0645d0f827fe
ioncrash <ioncrash@gmail.com>
2016-10-19 14:54:16 -0400

    reverse attempt 1
    Trying reverseFactorial function

--- reverseFactorial.js
+++ reverseFactorial.js
@@ -1,3 +1,10 @@
 function reverseFactorial(num) {
-  // happy coding!
+  let last = 1;
+  for (let i=1; i < num; i++) {
+    if (last === num) {
+      return i + "!";
+    }
+    last *= i;
+  }
+  return "None"
 }
