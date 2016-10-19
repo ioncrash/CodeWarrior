6a8e798b52efd6eb1fc214ad9713330248547bc4
ioncrash <ioncrash@gmail.com>
2016-10-19 15:41:59 -0400

    solve alternatecase
    solve alternatecase problem

--- alternateCase.js
+++ alternateCase.js
@@ -1,3 +1,12 @@
 function alternateCase(s) {
-  return '';
+  let altString = '';
+  let charArray = s.split('');
+  for (let i=0; i< charArray.length; i++) {
+    if (charArray[i] === charArray[i].toUpperCase()) {
+      altString = altString + charArray[i].toLowerCase();
+    } else if (charArray[i] === charArray[i].toLowerCase()) {
+      altString = altString + charArray[i].toUpperCase();
+    }
+  }
+  return altString;
 }
