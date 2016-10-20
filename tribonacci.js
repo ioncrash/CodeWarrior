fb976e7525f08ce544d9ea9e69695c5be734a823
ioncrash <ioncrash@gmail.com>
2016-10-20 16:38:29 -0400

    fix case of n <= 2
    add if statement to return truncated signature if n <= 2

--- tribonacci.js
+++ tribonacci.js
@@ -2,6 +2,11 @@ function tribonacci(signature,n){
   let seq = [];
   if (n === 0) {
     return seq;
+  } else if (n <= 2) {
+    for (let i = 0; i < n; i++) {
+      seq.push(signature[i]);
+    }
+    return seq;
   }
   let twoAgo = signature[0];
   let last = signature[1];
