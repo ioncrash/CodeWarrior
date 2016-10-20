0eb4a430b959613dd6c9c2eb1220cf42b4a116f7
ioncrash <ioncrash@gmail.com>
2016-10-20 16:55:24 -0400

    start parseImg
    start function to make sense of img names

--- takeAPicture.js
+++ takeAPicture.js
@@ -1,3 +1,15 @@
 function sortPhotos(pics){
-  //Make room for an amazing photo!
+  let newPics = [pics[0]];
+  let parseName = function(imgName) {
+    let noPeriod = imgName.slice('.');
+    return noPeriod;
+  }
+}
+
+
+  for (let i = 1; i < pics.length; i++) {
+    for (j = newPics.length - 1; j >= 0; j--) {
+      //add an if here to use the splice function, then a shift if length >5
+    }
+  }
 };
