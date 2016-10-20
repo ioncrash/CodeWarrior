07583789c170ccd5e4eda2dcc53334bfff53c0a5
ioncrash <ioncrash@gmail.com>
2016-10-20 16:25:19 -0400

    attempt 1 at trib
    attempt problem using thisOne, last, twoAgo, and next

--- tribonacci.js
+++ tribonacci.js
@@ -1,3 +1,19 @@
 function tribonacci(signature,n){
-  //your code here
+  let seq = [];
+  if (n === 0) {
+    return seq;
+  }
+  let twoAgo = signature[0];
+  let last = signature[1];
+  let thisOne = signature[2];
+  seq = signature;
+
+  for (let i = 2; i < n-1; i++) {
+    next = twoAgo + last + thisOne;
+    seq.push(next);
+    thisOne = next;
+    last = thisOne;
+    twoAgo = last;
+  }
+  return seq;
 }
