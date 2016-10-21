b27bc6d3f968b6c8ea75879e6be0fa21ce78b5e3
ioncrash <ioncrash@gmail.com>
2016-10-21 16:56:01 -0400

    add extra pic & slice array
    add extra pic and slice array to recent 5. Still failing tests??

--- takeAPicture2.js
+++ takeAPicture2.js
@@ -22,7 +22,16 @@ let sortImg = function(a,b) {
 
 function sortPhotos(pics){
   let newPics = pics.sort(sortImg);
+  let last = newPics[newPics.length - 1];
+  let lastYear = parseInt(last.slice(0,4));
+  let lastNum = parseInt(last.slice(last.length-1,last.length));
+  newPics.push(lastYear + ".img" + (lastNum + 1));
+
+  newPics = newPics.slice(newPics.length-5,newPics.length);
+
   return newPics;
+
+  //failing tests?
 }
 
 module.exports = {sortPhotos};
