2a453bca504db407035bb93f90d96f7f15c87081
ioncrash <ioncrash@gmail.com>
2016-10-21 14:51:00 -0400

    finish parseName, implement and cleanup
    finish parseName, implement and cleanup

--- takeAPicture.js
+++ takeAPicture.js
@@ -3,11 +3,25 @@ let parseName = function(imgName) {
   noPeriod[0] = parseInt(noPeriod[0]);
   noPeriod[1] = parseInt(noPeriod[1].charAt(noPeriod[1].length - 1));
   return noPeriod;
-}
+};
 
 function sortPhotos(pics){
   let newPics = [pics[0]];
+  for (let i = 1; i < pics.length; i++) {
+    //iterate through total pics
+    let thisOne = pics[i];
+    let thisSig = parseName(thisOne);
 
+    for (let j=(newPics.length - 1); j>=0; j--) {
+      let comparePic = newPics[j];
+      let compSig = parseName(comparePic);
+      //iterate through new memory bloc
+
+    }
+
+  }
+
+  //add thing to prevent size from exceeding 5
 }
 
 
