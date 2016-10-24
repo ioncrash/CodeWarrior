a534408f25eeb0995fc0b69b9a78706244dc613a
ioncrash <ioncrash@gmail.com>
2016-10-24 16:48:34 -0400

    use forEach to count directionals
    use forEach to count directionals

--- isValidWalk.js
+++ isValidWalk.js
@@ -1,3 +1,18 @@
+'use strict';
+
 function isValidWalk(walk) {
-  //insert brilliant code here
+  let counts = {
+    w : 0,
+    e : 0,
+    s : 0,
+    n : 0
+  };
+
+  walk.forEach( val => {
+    counts[val] += 1;
+  });
+
+  return counts;
 }
+
+module.exports = {isValidWalk};
