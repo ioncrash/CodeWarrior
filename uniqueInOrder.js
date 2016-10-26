47f27f79547e6767431fd25efe574e9571a2e268
ioncrash <ioncrash@gmail.com>
2016-10-26 15:24:09 -0400

    finish unique thing badjob
    finish unique thing badjob

--- uniqueInOrder.js
+++ uniqueInOrder.js
@@ -1,7 +1,21 @@
 'use strict';
 
 let uniqueInOrder=function(iterable){
-  //your code here - remember iterable can be a string or an array
+  let input;
+  if (typeof iterable === "string") {
+    input = iterable.split('');
+  } else {
+    input = iterable;
+  }
+
+  let alt = input.filter((e,i,a) => {
+    return e !== a[i-1];
+  });
+  // let result = '';
+  // alt.forEach((e,i,a) => {
+  //   result = result + e;
+  // });
+  return alt;
 };
 
 module.exports = {
