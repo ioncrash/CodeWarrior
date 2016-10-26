bbf47fb227c7533fc67199cb829552081dc3ba36
ioncrash <ioncrash@gmail.com>
2016-10-26 14:33:19 -0400

    complete names function
    complete names function

--- formatNames.js
+++ formatNames.js
@@ -1,3 +1,20 @@
+'use strict';
+
 function list(names){
-  //your code here
+  let result = names[0].name;
+
+  let endIndex = names.length - 1;
+
+  for (let i = 1; i < names.length; i++) {
+    if (i === endIndex) {
+      result = result + " & " + names[i].name;
+    } else {
+      result = result + ", " + names[i].name;
+    }
+  }
+  return result;
 }
+
+module.exports = {
+  list
+};
